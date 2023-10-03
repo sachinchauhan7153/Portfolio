52dcd4bf4641706957f388e37447c091c980c0bd
Sachin Chauhan <136893598+sachinchauhan7153@users.noreply.github.com>
2023-10-03 09:34:16 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================= Typing Animation =========================== */
+var typed = new Typed(".typing",{
+    strings:["","Fresher","Web Designer","Web Developer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
